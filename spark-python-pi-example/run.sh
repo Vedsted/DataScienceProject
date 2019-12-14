8eccc0e1ce4b7d607abbc186f9c410cab87b62ba
Christian Clausen <csbc92@hotmail.com>
2019-12-14 12:22:25 +0100

    Renamed containers in order to be to run all examples simultaneously

--- spark-python-pi-example/run.sh
+++ spark-python-pi-example/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --rm -t spark-python-example .
-docker run --rm --name spark-python-example --network hadoop --ip 172.25.0.240 spark-python-example
+docker build --rm -t spark-python-example-1 .
+docker run --rm --name spark-python-example-1 --network hadoop --ip 172.25.0.241 spark-python-example-1
--- spark-python-word-count-example/run.sh
+++ spark-python-word-count-example/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --rm -t spark-python-example .
-docker run --rm --name spark-python-example --network hadoop --ip 172.25.0.240 spark-python-example
+docker build --rm -t spark-python-example-2 .
+docker run --rm --name spark-python-example-2 --network hadoop --ip 172.25.0.242 spark-python-example-2
