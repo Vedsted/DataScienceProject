dc18c8d8f10cba6766ee2516c45c4aea8b097dad
Victor <victor.s@live.dk>
2019-12-18 08:16:16 +0100

    WEBSERVER WIP

--- webserver/src/app.ts
+++ webserver/src/app.ts
@@ -1,16 +1,40 @@
 import express = require('express');
-import {spawn} from "child_process" ;
-
+import { spawn } from "child_process";
+import * as request from "request-promise-native";
 // Create a new express application instance
 const app: express.Application = express();
 
-app.get('/', function (req, res) {
-    
-
-    const pythonProcess = spawn('python',["./src/python/test.py"]);
+app.get('/', async function (req, res) {
 
 
-    pythonProcess.stdout.on('data', (data:Buffer) => {
+    request.post("http://192.168.43.61:8047/query.json", {
+        json: {
+            queryType: "SQL",
+           // query: "SELECT * FROM dfs.`dataframes/street.csv`",
+            query: "SELECT * FROM dfs.`dataframes/street.csv` WHERE Crime_type='Drugs'",
+            autoLimit: "10000"
+        }
+    }, (error, result, body) => {
+        if (error) {
+            console.error(error)
+            return
+        }
+        console.log(`statusCode: ${result.statusCode}`)
+        console.log(body)
+        console.log(body)
+        res.send(body);
+    })
+  
+
+});
+
+app.get('/python', function (req, res) {
+
+
+    const pythonProcess = spawn('python', ["./src/python/test.py"]);
+
+
+    pythonProcess.stdout.once('data', (data: Buffer) => {
         // Do something with the data returned from python script
         console.log(data.toString());
         console.log(data);
--- webserver/src/python/test.py
+++ webserver/src/python/test.py
@@ -1,16 +1,23 @@
 import sys
+import os
 from pyspark.sql import SparkSession
 
 spark = SparkSession \
     .builder \
+    .master("local")\
     .appName("Python Spark SQL basic example") \
-    .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
-df = spark.read.json("./src/python/people.json")
-# Displays the content of the DataFrame to stdout
-#df.show()
+
+path = os.path.join(os.getcwd(), "src", "python", "people.json")
+print(path)
+df = spark.read.json(path)
+#Displays the content of the DataFrame to stdout
+df.show()
 
 dataToSendBack = df.toJSON()
+
+spark.stop()
 print(dataToSendBack)
 sys.stdout.flush()
+
