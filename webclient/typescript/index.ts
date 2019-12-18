fea97198e4967ec61670da6f750476b6a767fab7
Victor <victor.s@live.dk>
2019-12-18 11:25:52 +0100

    (maybe not) working index.ts

--- webclient/typescript/index.ts
+++ webclient/typescript/index.ts
@@ -12,45 +12,56 @@ const radius = document.getElementById('radius') as HTMLInputElement;
 const radiusValue = document.getElementById('radiusValue') as HTMLElement;
 const blurValue = document.getElementById('blurValue') as HTMLElement;
 
-var vector = new HeatmapLayer({
-    source: new VectorSource({
-        url: 'https://christianclausen.dk/crimedata.geojson',
-        format: new GeoJSON()
-    }),
-    blur: parseInt(blur.value, 25),
-    radius: parseInt(radius.value, 10),
-    weight: function(feature) {
-        var weightProperty = feature.get('weight'); // The key from the properties object in the GeoJSON object
-        var weight = parseFloat(weightProperty);
-        return weight;
-    }
-});
+let res= fetch("http://localhost:3000/raw", {method:"POST", body:JSON.stringify({ columns:["Latitude", "Longitude"], filePath:"street", params:[{key:"Crime_type", value:"Drugs" }]})})
+res.then((res) => {
+    return res.json()
+}).then((json) => {
+    var vector = new HeatmapLayer({
+        source: new VectorSource({
+            format: new GeoJSON(),
+            features: JSON.parse(json).features
+    
+        }),
+        blur: parseInt(blur.value, 25),
+        radius: parseInt(radius.value, 10),
+        weight: function(feature) {
+            var weightProperty = feature.get('weight'); // The key from the properties object in the GeoJSON object
+            var weight = parseFloat(weightProperty);
+            return weight;
+        }
+    });
+    
+    var raster = new TileLayer({
+        source: new Stamen({
+            layer: 'toner'
+        })
+    });
+    
+    new Map({
+        layers: [raster, vector],
+        target: 'map',
+        view: new View({
+            center: fromLonLat([-0.096091, 51.51391]), // Approx. Center of London
+            zoom: 15
+        })
+    });
+    
+    var blurHandler = function() {
+        vector.setBlur(parseInt(blur.value, 10));
+        blurValue.innerHTML = blur.value;
+    };
+    blur.addEventListener('input', blurHandler);
+    blur.addEventListener('change', blurHandler);
+    
+    var radiusHandler = function() {
+        vector.setRadius(parseInt(radius.value, 10));
+        radiusValue.innerHTML = radius.value;
+    };
+    radius.addEventListener('input', radiusHandler);
+    radius.addEventListener('change', radiusHandler);
+})
 
-var raster = new TileLayer({
-    source: new Stamen({
-        layer: 'toner'
-    })
-});
 
-new Map({
-    layers: [raster, vector],
-    target: 'map',
-    view: new View({
-        center: fromLonLat([-0.096091, 51.51391]), // Approx. Center of London
-        zoom: 15
-    })
-});
 
-var blurHandler = function() {
-    vector.setBlur(parseInt(blur.value, 10));
-    blurValue.innerHTML = blur.value;
-};
-blur.addEventListener('input', blurHandler);
-blur.addEventListener('change', blurHandler);
 
-var radiusHandler = function() {
-    vector.setRadius(parseInt(radius.value, 10));
-    radiusValue.innerHTML = radius.value;
-};
-radius.addEventListener('input', radiusHandler);
-radius.addEventListener('change', radiusHandler);
+
