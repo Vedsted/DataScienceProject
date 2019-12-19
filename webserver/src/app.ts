c86bb7a9fba82728968ed9536addc1a2dc91168d
Victor <victor.s@live.dk>
2019-12-19 08:20:03 +0100

    minor webserver fixes

--- webserver/src/app.ts
+++ webserver/src/app.ts
@@ -1,24 +1,29 @@
 import express = require('express');
-import { spawn } from "child_process";
-import { RawQuerySpecification } from './RawQuerySpecification';
+import { RawQuerySpecification } from './rawQuerySpecification';
 import { SqlBuilder } from './sqlBuilder';
 import { DrillAdapter } from './drillAdapter';
-import { GeoJsonConverter, convertToGeoJson } from './geoJsonConverter';
+import { convertToGeoJson } from './geoJsonConverter';
+import  cors from "cors";
 const bodyParser = require('body-parser');
 // Create a new express application instance
 const app: express.Application = express();
 app.use(bodyParser.json())
+app.use(cors());
+
+
 
 app.post('/raw', async function (req : express.Request, res : express.Response) {
+    console.log(req.body);
 
     try{
 
         let request = new RawQuerySpecification(req.body);
         let sqlBuilder = new SqlBuilder();
-        let sqlString = sqlBuilder.select(request.columns).from(request.filePath).where(request.params).limit(100).collect();
+        let sqlString = sqlBuilder.select(request.columns).from(request.filePath).where(request.params).limit(1000000).collect();
         console.log(sqlString);
         let drillAdapter = new DrillAdapter("192.168.43.61:8047");
         let result = await drillAdapter.performRequest(sqlString);
+        console.log(result.rows.length)
         let geoJson = convertToGeoJson(result.rows)
         res.send(geoJson)
     }catch (error){
--- webserver/src/geoJsonConverter.ts
+++ webserver/src/geoJsonConverter.ts
@@ -2,7 +2,7 @@ export function convertToGeoJson(rows: any[]) {
     return  {
         type: "FeatureCollection", 
         features: rows.map(row => {
-            return { type: "Feature", geometry: { type: "Point", coordinates: [parseFloat(row.Latitude), parseFloat(row.Longitude)] }, properties: { weight: 1, } }
+            return { type: "Feature", geometry: { type: "Point", coordinates: [parseFloat(row.Longitude),parseFloat(row.Latitude)] }, properties: { weight: 1 } }
         })
     }
 }
